5efcf897b386b5d4e4db7578a1c4cca07860b043
יהודית טמינו <taminojudit@hotmail.co.il>
2025-05-27 17:25:09 +0300

    Express Async Handler

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,42 +1,44 @@
+const asyncHandler = require('express-async-handler');
+
 // @description Get Contacts
 // @route GET/api/contacts
 // @access public
-const getContacts = (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Get all contacts' });
-}
+})
 
 // @description Create Contact
 // @route POST/api/contacts
 // @access public
-const createContact = (req, res) => {
-  const {name, email, phone} = req.body;
+const createContact = asyncHandler(async (req, res) => {
+  const { name, email, phone } = req.body;
 
-  if(!name || !email || !phone) {
+  if (!name || !email || !phone) {
     res.status(400);
     throw new Error("All fileds are require");
   }
   res.status(201).json({ message: 'Create contact' });
-}
+})
 
 // @description Update Contact
 // @route PUT/api/contacts/:id
 // @access public
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update contact ${req.params.id}` });
-}
+})
 
 // @description Delete Contact
 // @route DELETE/api/contacts/:id
 // @access public
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete contact ${req.params.id}` });
-}
+})
 
 // @description Get Contact
 // @route GET/api/contacts/:id
 // @access public
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Get contact ${req.params.id}` });
-}
+})
 
 module.exports = { getContacts, createContact, updateContact, deleteContact, getContact }
